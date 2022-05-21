7c882a49fc31d8ead2400408204986ceaecd970a
Alexis Degraeve <alexis.degraeve@hotmail.com>
2022-05-21 14:47:11 +0200

    add redirect

--- potatosales/src/app/app-routing.module.ts
+++ potatosales/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pscommon/pagenotfound/pagenotfound.component';
 import { WelcomeComponent } from './sales/welcome/welcome.component';
 
 const routes: Routes = [
+  { path: '', component: WelcomeComponent },
   { path: 'welcome', component: WelcomeComponent },
+  { path: '**', component: PagenotfoundComponent }, 
 ];
 
 @NgModule({
--- potatosales/src/app/pscommon/pscommon.module.ts
+++ potatosales/src/app/pscommon/pscommon.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PagenotfoundComponent
   ],
   imports: [
     CommonModule
